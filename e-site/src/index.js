47d4021628c26f2208177373af973abf9123a57a
DianaWangui <dianawanguikt048@gmail.com>
2024-12-15 18:56:55 +0300

    added the cCartProvider for the custom state created for the items in the cart

--- e-site/src/index.js
+++ e-site/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { CartProvider } from './context/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+    <CartProvider>
     <App />
-  </React.StrictMode>
+  </CartProvider>,
 );
 reportWebVitals();
